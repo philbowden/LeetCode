429e732a7bb922a86d1fcb0652d153601135fd9a
Phil Bowden <46327811+philbowden@users.noreply.github.com>
2024-06-07 13:08:17 -0700

    Time: 2 ms (6.99%), Space: 43.1 MB (28.59%) - LeetHub

--- 0112-path-sum/0112-path-sum.java
+++ 0112-path-sum/0112-path-sum.java
@@ -14,11 +14,30 @@
  * }
  */
 class Solution {
-    public boolean hasPathSum(TreeNode root, int sum) {
+    public boolean hasPathSum(TreeNode root, int targetSum) {
         if (root == null) return false;
-
-        sum -= root.val;
-        if ((root.left == null) && (root.right == null)) return (sum == 0);
-        return hasPathSum(root.left, sum) || hasPathSum(root.right, sum);
+        
+        Stack<TreeNode> nodeStack = new Stack<>();
+        Stack<Integer> sumStack = new Stack<>();
+        nodeStack.push(root);
+        sumStack.push(targetSum - root.val);
+        
+        while(!nodeStack.isEmpty()) {
+            TreeNode node = nodeStack.pop();
+            int currentSum = sumStack.pop();
+            
+            if (node.left == null && node.right == null && currentSum == 0) {
+                return true;
+            } 
+            if (node.left != null) {
+                sumStack.push(currentSum - node.left.val);
+                nodeStack.push(node.left);
+            }
+            if (node.right != null) {
+                sumStack.push(currentSum - node.right.val);
+                nodeStack.push(node.right);
+            }
+        }
+        return false;
     }
 }
