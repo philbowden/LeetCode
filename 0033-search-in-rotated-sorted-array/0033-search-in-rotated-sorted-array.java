99853fde52643784f7cfb650b93d21d16be940a2
Phil Bowden <46327811+philbowden@users.noreply.github.com>
2024-06-08 14:12:25 -0700

    Time: 0 ms (100%), Space: 41.5 MB (96.38%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -6,24 +6,29 @@ class Solution {
             int mid = left + (right - left) / 2;
 
             if (nums[mid] == target) return mid;
-            else if (nums[mid] > nums[right]) left = mid+1;
+            else if (nums[mid] > nums[right]) left = mid + 1;
             else right = mid;
         }
-
         int start = left;
         left = 0;
-        right = n-1;
-
-        if (nums[start] <= target && target <= nums[right]) {
-            left = start;
-        } else right = start;
-
+        right = nums.length-1;
+        if (nums[start] <= target && target <= nums[right]) left = start;
+        else right = start;
         while(left <= right) {
             int mid = left + (right - left) / 2;
+
             if (nums[mid] == target) return mid;
-            else if (nums[mid] < target) left = mid+1;
-            else right = mid-1;
+            else if (nums[mid] < target) left = mid + 1;
+            else right = mid-1; 
         }
         return -1;
     }
+    /*
+    [4,5,6,7,0,1,2] target = 6
+     l
+             s
+                 r
+    
+    
+    */ 
 }
