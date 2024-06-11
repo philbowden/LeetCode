cc023a5a4ffc26a52fb2c33c2f0d1db7ce7b95eb
Phil Bowden <46327811+philbowden@users.noreply.github.com>
2024-06-10 19:35:46 -0700

    Time: 0 ms (100%), Space: 42.4 MB (66.9%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
@@ -14,11 +14,17 @@
  * }
  */
 class Solution {
+    int max = 0;
     public int maxDepth(TreeNode root) {
-        if (root == null) return 0;
+        if (root == null) return max;
+        dfs(root, 0);
+        return max + 1;
+    }
+    private void dfs(TreeNode root, int depth) {
+        if (root == null) return;
 
-        int left = maxDepth(root.left);
-        int right = maxDepth(root.right);
-        return Math.max(left, right) + 1;
+        max = Math.max(max, depth);
+        dfs(root.left, depth+1);
+        dfs(root.right, depth+1);
     }
 }
