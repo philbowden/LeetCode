9df9e8b78b22121ce075c364d3a42fab159c845c
Phil Bowden <46327811+philbowden@users.noreply.github.com>
2024-06-10 20:24:40 -0700

    Time: 0 ms (100%), Space: 44 MB (87.66%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.java
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.java
@@ -14,18 +14,18 @@
  * }
  */
 class Solution {
-    boolean isTreeBalanced = true;
+    boolean isBalanced = true;
     public boolean isBalanced(TreeNode root) {
-        if (root == null) return true;
+        if (root == null) return isBalanced;
         dfs(root);
-        return isTreeBalanced;
+        return isBalanced;
     }
     private int dfs(TreeNode node) {
-        if (node == null) return 0;
-
+        if (node == null) return 1;
         int left = dfs(node.left);
         int right = dfs(node.right);
-        if (Math.abs(left - right) > 1) isTreeBalanced = false;
+
+        if (Math.abs(left - right) > 1) isBalanced = false;
         return Math.max(left, right) + 1;
     }
 }
