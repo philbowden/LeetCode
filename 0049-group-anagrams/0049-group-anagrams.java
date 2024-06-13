909ce6ff4d4977933778976da1d1cf8b18411ba8
Phil Bowden <46327811+philbowden@users.noreply.github.com>
2024-06-13 10:32:25 -0700

    Time: 8 ms (38.44%), Space: 47.4 MB (83.79%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -0,0 +1,24 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        List<List<String>> res = new ArrayList<>();
+        for (String s : strs) {
+            String key = getKey(s);
+            if (!map.containsKey(key)) map.put(key, new ArrayList<>());
+            map.get(key).add(s);
+        }
+        for (Map.Entry<String, List<String>> entry : map.entrySet()) {
+            res.add(entry.getValue());
+        }
+        return res;
+    }
+    private String getKey(String s) {
+        char[] characters = s.toCharArray();
+        Arrays.sort(characters);
+        StringBuilder sb = new StringBuilder();
+        for (char c : characters) {
+            sb.append(c);
+        }
+        return sb.toString();
+    }
+}
