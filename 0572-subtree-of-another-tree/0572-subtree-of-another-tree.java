7262fe875f107bfedeb6ca279bc3eb33e1ec1d66
Phil Bowden <46327811+philbowden@users.noreply.github.com>
2024-07-05 22:36:58 -0700

    Time: 2 ms (96.59%), Space: 44.3 MB (29.74%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.java
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.java
@@ -19,10 +19,10 @@ class Solution {
         if (sameTree(root, subRoot)) return true;
         return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
     }
-    private boolean sameTree(TreeNode root, TreeNode subRoot) {
-        if (root == null && subRoot == null) return true;
-        if (root == null || subRoot == null) return false;
-        if (root.val != subRoot.val) return false;
-        return sameTree(root.left, subRoot.left) && sameTree(root.right, subRoot.right);
+    private boolean sameTree(TreeNode node, TreeNode subRoot) {
+        if (node == null && subRoot == null) return true;
+        if (node == null | subRoot == null) return false;
+        if (node.val != subRoot.val) return false;
+        return sameTree(node.left, subRoot.left) && sameTree(node.right, subRoot.right);
     }
 }
