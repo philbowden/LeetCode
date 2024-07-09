5edc361df946f65e2b03cbc2afa9d14a8eebec46
Phil Bowden <46327811+philbowden@users.noreply.github.com>
2024-07-09 15:02:02 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.java
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -3,13 +3,13 @@ class Solution {
         int n = nums.length; 
 
         double pre = 1, suff = 1;
-        int ans = Integer.MIN_VALUE;
+        int max = Integer.MIN_VALUE;
         for (int i = 0; i < n; i++) {
             if (pre == 0) pre = 1;
             if (suff == 0) suff = 1;
             pre *= (double)nums[i];
             suff *= (double)nums[n - i - 1];
-            ans = (int)Math.max(ans, Math.max(pre, suff));
+            max = (int)Math.max(max, Math.max(pre, suff));
         }
         return ans;
     }
