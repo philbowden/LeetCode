1651c9eca7fc2fd4bb2c686fecb9242ab7bcabd9
Phil Bowden <46327811+philbowden@users.noreply.github.com>
2024-07-09 11:53:22 -0700

    Time: 0 ms (100%), Space: 44.2 MB (60.08%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.java
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.java
@@ -14,31 +14,20 @@
  * }
  */
 class Solution {
-    class TreeInfo {
-        int depth;
-        boolean isBalanced;
-        public TreeInfo(int depth, boolean isBalanced) {
-            this.depth = depth;
-            this.isBalanced = isBalanced;
-        }
-        public TreeInfo() {};
-    }
+    boolean isTreeBalanced = true;
     public boolean isBalanced(TreeNode root) {
-        if (root == null) return true;
-        return dfs(root).isBalanced;
+        if (root == null) return isTreeBalanced;
+        dfs(root);
+        return isTreeBalanced;
     }
-    private TreeInfo dfs(TreeNode node) {
-        if (node == null) return new TreeInfo(0, true);
+    private int dfs(TreeNode root) {
+        if (root == null) return 0;
 
-        TreeInfo left = dfs(node.left);
-        TreeInfo right = dfs(node.right);
+        int left = dfs(root.left);
+        int right = dfs(root.right);
 
-        int diff = Math.abs(left.depth - right.depth);
-        int currentDepth = Math.max(left.depth, right.depth) + 1;
-        TreeInfo res = new TreeInfo(currentDepth, true);
-        if (diff > 1 || !left.isBalanced || !right.isBalanced) {
-            res.isBalanced = false;
-        }
-        return res;
+        int diff = Math.abs(left - right);
+        if (diff > 1) isTreeBalanced = false;
+        return Math.max(left, right) + 1;
     }
 }
