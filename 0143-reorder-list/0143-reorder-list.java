e4149e5e55271aa3f921985c25c8ed5163df033f
Phil Bowden <46327811+philbowden@users.noreply.github.com>
2024-07-16 15:29:42 -0700

    Time: 2 ms (81.94%), Space: 48.4 MB (17.19%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.java
+++ 0143-reorder-list/0143-reorder-list.java
@@ -14,16 +14,16 @@ public void reorderList(ListNode head) {
 
         // find the middle of linked list [Problem 876]
         // in 1->2->3->4->5->6 find 4
-        ListNode slow = head, fast = head;
+        ListNode mid = head, fast = head;
         while (fast != null && fast.next != null) {
-            slow = slow.next;
+            mid = mid.next;
             fast = fast.next.next;
         }
 
         // reverse the second part of the list [Problem 206]
         // convert 1->2->3->4->5->6 into 1->2->3->4 and 6->5->4
         // reverse the second half in-place
-        ListNode prev = null, curr = slow, tmp = null;
+        ListNode prev = null, curr = mid, tmp = null;
         while (curr != null) {
             tmp = curr.next;
 
