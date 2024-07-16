2071df0aa374835a04162cf55536b7e9c4e204e5
Phil Bowden <46327811+philbowden@users.noreply.github.com>
2024-07-16 16:20:13 -0700

    Time: 1 ms (99.8%), Space: 48 MB (57.97%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.java
+++ 0143-reorder-list/0143-reorder-list.java
@@ -9,40 +9,28 @@
  * }
  */
 class Solution {
-public void reorderList(ListNode head) {
-        if (head == null) return;
-
-        // find the middle of linked list [Problem 876]
-        // in 1->2->3->4->5->6 find 4
+    public void reorderList(ListNode head) {
         ListNode mid = head, fast = head;
-        while (fast != null && fast.next != null) {
+        while(fast != null && fast.next != null) {
             mid = mid.next;
             fast = fast.next.next;
         }
-
-        // reverse the second part of the list [Problem 206]
-        // convert 1->2->3->4->5->6 into 1->2->3->4 and 6->5->4
-        // reverse the second half in-place
-        ListNode prev = null, curr = mid, tmp = null;
-        while (curr != null) {
-            tmp = curr.next;
-
-            curr.next = prev;
-            prev = curr;
-            curr = tmp;
+        ListNode cur = mid, prev = null, temp = null;
+        while(cur != null) {
+            temp = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = temp;
         }
-
-        // merge two sorted linked lists [Problem 21]
-        // merge 1->2->3->4 and 6->5->4 into 1->6->2->5->3->4
-        ListNode first = head, second = prev;
-        while (second.next != null) {
-            tmp = first.next;
-            first.next = second;
-            first = tmp;
-
-            tmp = second.next;
-            second.next = first;
-            second = tmp;
+        ListNode l1 = head, l2 = prev;
+        while(l2.next != null) {
+            temp = l1.next;
+            l1.next = l2;
+            l1 = temp;
+            
+            temp = l2.next;
+            l2.next = l1;
+            l2 = temp;
         }
     }
 }
