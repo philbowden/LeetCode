4ee17881d15af04fe93920055e0f800f87cc0653
Phil Bowden <46327811+philbowden@users.noreply.github.com>
2024-07-17 11:55:23 -0700

    Time: 5 ms (37.35%), Space: 44.5 MB (74.22%) - LeetHub

--- 0155-min-stack/0155-min-stack.java
+++ 0155-min-stack/0155-min-stack.java
@@ -1,29 +1,25 @@
 class MinStack {
-
-    private Stack<Integer> stack = new Stack<>();
     private Stack<Integer> mins = new Stack<>();
+    private Stack<Integer> stack = new Stack<>();
 
     public MinStack() {}
     
-
     public void push(int val) {
-        if (mins.isEmpty() || val <= mins.peek()) {
+        if (mins.isEmpty() || mins.peek() >= val) {
             mins.push(val);
         }
         stack.push(val);
     }
-
+    
     public void pop() {
-       if (!mins.isEmpty() && stack.peek().equals(mins.peek())) {
-         mins.pop();
-       }
-       stack.pop();
+        if (!mins.isEmpty() && stack.peek().equals(mins.peek())) mins.pop();
+        stack.pop();
     }
-
+    
     public int top() {
         return stack.peek();
     }
-
+    
     public int getMin() {
         return mins.peek();
     }
