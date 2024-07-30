d5354ce0c3889429eac235908b15aa502cc0e3a7
Phil Bowden <46327811+philbowden@users.noreply.github.com>
2024-07-29 20:54:22 -0700

    Time: 7 ms (31.01%), Space: 45.6 MB (25.19%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -3,35 +3,34 @@ class Solution {
     Set<Integer> set = new HashSet<>();
 
     public boolean canFinish(int numCourses, int[][] prerequisites) {
+        //store courses and prereqs into map
         for (int[] node : prerequisites) {
             if (!map.containsKey(node[0])) map.put(node[0], new ArrayList<>());
-            List<Integer> list = map.get(node[0]);
-            list.add(node[1]);
-            map.put(node[0], list);
+            List<Integer> prereqs = map.get(node[0]);
+            prereqs.add(node[1]);
         }
+        //loop through each class and make sure it doesn't have a cycle
         for (int i = 0; i < numCourses; i++) {
             if (!dfs(i)) return false;
         }
         return true;
     }
     private boolean dfs(int course) {
-        if (set.contains(course)) {
-            return false;
-        }
-        if (map.containsKey(course) && map.get(course).isEmpty()) {
-            return true;
-        }
+        //if the set already has the course then there's a cycle
+        if (set.contains(course)) return false;
+
+        //if the map contains the course but value is empty then it doesn't have dependencies
+        if (map.containsKey(course) && map.get(course).isEmpty()) return true;
 
         set.add(course);
-        List<Integer> prereqs = new ArrayList<>();
-        if (map.containsKey(course)) {
-            prereqs = map.get(course);
-        }
-        for (int c : prereqs) {
+        List<Integer> courses = new ArrayList<>();
+        if (map.containsKey(course)) courses = map.get(course);
+        for (int c : courses) {
             if (!dfs(c)) return false;
-        }
-        set.remove(course);
-        map.put(course, new ArrayList<>());
-        return true;
+            }
+            //if course doesn't have cycle then remove it from set and give it an empty list in map
+            set.remove(course);
+            map.put(course, new ArrayList<>());
+            return true;
     }
 }
