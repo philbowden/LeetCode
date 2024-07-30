6e523a6b6fe88ef8feaff0b9057d32fef7782699
Phil Bowden <46327811+philbowden@users.noreply.github.com>
2024-07-29 21:07:30 -0700

    Time: 16 ms (23.17%), Space: 45 MB (6.98%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,5 +1,6 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) return false;
         Map<Character, Integer> map = new HashMap<>();
         for (char c : s.toCharArray()) {
             map.put(c, map.getOrDefault(c, 0) + 1);
@@ -7,8 +8,9 @@ class Solution {
         for (char c : t.toCharArray()) {
             if (!map.containsKey(c)) return false;
             else map.put(c, map.get(c) - 1);
+
             if (map.get(c) == 0) map.remove(c);
         }
-        return map.size() == 0;
+        return true;
     }
 }
