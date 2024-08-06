59f0a5e75302c9c4572154df648014eed14ed4ae
Phil Bowden <46327811+philbowden@users.noreply.github.com>
2024-08-06 15:15:33 -0700

    Time: 14 ms (23.91%), Space: 44.8 MB (28.37%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,27 +1,17 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        Map<String, Set<Character>> map = new HashMap<>();
-        String row = "";
-        String col = "";
-        String square = "";
+        Set<String> set = new HashSet<>();
+
         for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[i].length; j++) {
+            for (int j = 0; j < board[0].length; j++) {
                 char current = board[i][j];
                 if (current != '.') {
-                    row = "row " + i;
-                    col = "col " + j;
-                    square = "square [" + i/3 +", " + j/3 +"]";
-                    if (!map.containsKey(row)) map.put(row, new HashSet<>());
-                    if(!map.get(row).add(current)) return false;
-
-                    if (!map.containsKey(col)) map.put(col, new HashSet<>());
-                    if (!map.get(col).add(current)) return false;
-
-                    if (!map.containsKey(square)) map.put(square, new HashSet<>());
-                    if (!map.get(square).add(current)) return false;
+                   if (!set.add("row " + i + " -> " + current)) return false;
+                   if (!set.add("col " + j + " -> " + current)) return false;
+                   if (!set.add("box [" + i/3 + ", " + j/3 + "] -> " +  current)) return false;
+                }
             }
         }
-      }
         return true;
     }
 }
