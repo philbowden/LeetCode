84792907a2077c9c854b656b1904b73d381f0283
Phil Bowden <46327811+philbowden@users.noreply.github.com>
2025-03-16 21:56:03 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -2,14 +2,14 @@ class Solution {
     public boolean isAnagram(String s, String t) {
         Map<Character, Integer> map = new HashMap<>();
         for (char c : s.toCharArray()) {
-            map.put(c, map.getOrDefault(c, 0) + 1);
+            if (map.containsKey(c)) map.put(c, map.get(c)+1);
+            else map.put(c, 1);
         }
         for (char c : t.toCharArray()) {
             if (!map.containsKey(c)) return false;
             else map.put(c, map.get(c) - 1);
-
             if (map.get(c) == 0) map.remove(c);
         }
-        return map.size() == 0;
+        return true;
     }
 }
