cff5756a679ac53800b92be22da5bd0f387330a0
Phil Bowden <46327811+philbowden@users.noreply.github.com>
2025-03-29 14:59:38 -0700

    Time: 7 ms (51.63%), Space: 45.9 MB (24.01%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,36 +1,38 @@
 class Solution {
-    Map<Integer, List<Integer>> map = new HashMap<>();
-    Set<Integer> set = new HashSet<>();
-
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        //store courses and prereqs into map
-        for (int[] node : prerequisites) {
-            if (!map.containsKey(node[0])) map.put(node[0], new ArrayList<>());
-            List<Integer> prereqs = map.get(node[0]);
-            prereqs.add(node[1]);
-        }
-        //loop through each class and make sure it doesn't have a cycle
+        Map<Integer, List<Integer>> graph = new HashMap<>();
+        int[] indegrees = new int[numCourses];
+
         for (int i = 0; i < numCourses; i++) {
-            if (!dfs(i)) return false;
+            graph.put(i, new ArrayList<>());
         }
-        return true;
-    }
-    private boolean dfs(int course) {
-        //if the set already has the course then there's a cycle
-        if (set.contains(course)) return false;
 
-        //if the map contains the course but value is empty then it doesn't have dependencies
-        if (map.containsKey(course) && map.get(course).isEmpty()) return true;
+        for (int[] pre : prerequisites) {
+            graph.get(pre[1]).add(pre[0]);
+            ++indegrees[pre[0]];
+        }
 
-        set.add(course);
-        List<Integer> courses = new ArrayList<>();
-        if (map.containsKey(course)) courses = map.get(course);
-        for (int c : courses) {
-            if (!dfs(c)) return false;
+        int completedCourses = 0;
+        Queue<Integer> que = new LinkedList<>();
+        for (int i = 0; i < indegrees.length; i++) {
+            if (indegrees[i] ==0) que.add(i);
+        }
+
+        while(!que.isEmpty()) {
+            int course = que.poll();
+            for (int dependentCourse : graph.get(course)) {
+                if (--indegrees[dependentCourse] == 0) {
+                    que.add((dependentCourse));
+                }
             }
-            //if course doesn't have cycle then remove it from set and give it an empty list in map
-            set.remove(course);
-            map.put(course, new ArrayList<>());
-            return true;
+            ++completedCourses;
+        }
+
+        return completedCourses == numCourses;
     }
 }
+/*
+
+
+
+*/ 
