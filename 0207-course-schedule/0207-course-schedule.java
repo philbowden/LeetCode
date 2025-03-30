5de8104940b473ec8d5f8b5ee74b4a0fd1cbf806
Phil Bowden <46327811+philbowden@users.noreply.github.com>
2025-03-30 16:45:26 -0700

    Time: 8 ms (34.79%), Space: 46.3 MB (10.58%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -2,37 +2,30 @@ class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
         Map<Integer, List<Integer>> graph = new HashMap<>();
         int[] indegrees = new int[numCourses];
-
+        Queue<Integer> q = new LinkedList<>();
         for (int i = 0; i < numCourses; i++) {
             graph.put(i, new ArrayList<>());
         }
-
         for (int[] pre : prerequisites) {
-            graph.get(pre[1]).add(pre[0]);
-            ++indegrees[pre[0]];
+           graph.get(pre[1]).add(pre[0]);
+           indegrees[pre[0]]++;
         }
 
-        int completedCourses = 0;
-        Queue<Integer> que = new LinkedList<>();
-        for (int i = 0; i < indegrees.length; i++) {
-            if (indegrees[i] ==0) que.add(i);
-        }
-
-        while(!que.isEmpty()) {
-            int course = que.poll();
-            for (int dependentCourse : graph.get(course)) {
-                if (--indegrees[dependentCourse] == 0) {
-                    que.add((dependentCourse));
-                }
+        for (int i = 0; i < numCourses; i++) {
+            if (indegrees[i] == 0) {
+                q.add(i);
             }
-            ++completedCourses;
         }
-
+        int completedCourses = 0;
+        while(!q.isEmpty()) {
+            int course = q.poll();
+            List<Integer> dependentCourses = graph.get(course);
+            for (int dependent : dependentCourses) {
+                --indegrees[dependent];
+                if (indegrees[dependent] == 0) q.add(dependent);
+            }
+            completedCourses++;
+        }
         return completedCourses == numCourses;
     }
 }
-/*
-
-
-
-*/ 
