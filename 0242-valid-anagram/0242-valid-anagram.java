07e6c8ad4f581d16c2d40575e9711d455d86c67d
Phil Bowden <46327811+philbowden@users.noreply.github.com>
2025-05-07 18:56:45 -0700

    Time: 20 ms (12.42%), Space: 44.7 MB (44.31%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,15 +1,19 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
         Map<Character, Integer> map = new HashMap<>();
-        for (char c : s.toCharArray()) {
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
             if (map.containsKey(c)) map.put(c, map.get(c)+1);
             else map.put(c, 1);
         }
-        for (char c : t.toCharArray()) {
+        for (int i = 0; i < t.length(); i++) {
+            char c = t.charAt(i);
             if (!map.containsKey(c)) return false;
-            else map.put(c, map.get(c) - 1);
-            if (map.get(c) == 0) map.remove(c);
+            else {
+                map.put(c, map.get(c) - 1);
+                if (map.get(c) == 0) map.remove(c);
+            }
         }
-        return true;
+        return map.isEmpty();
     }
 }
