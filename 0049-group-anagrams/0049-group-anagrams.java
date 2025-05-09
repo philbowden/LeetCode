500a46958ff2c83283c0358f44ae7baecb5fd294
Phil Bowden <46327811+philbowden@users.noreply.github.com>
2025-05-08 17:03:37 -0700

    Time: 8 ms (40.04%), Space: 47.7 MB (90.75%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,23 +1,29 @@
 class Solution {
-    public List<List<String>> groupAnagrams(String[] strs) {
+public List<List<String>> groupAnagrams(String[] strs) {
         Map<String, List<String>> map = new HashMap<>();
-        List<List<String>> res = new ArrayList<>();
-        for (String str : strs) {
-            char[] characters = str.toCharArray();
-            Arrays.sort(characters);
-            StringBuilder s = new StringBuilder();
-            for (char c : characters) {
-                s.append(c);
+        for (String s : strs) {
+                String key = makeKey(s);
+                if (!map.containsKey(key)) {
+                    map.put(key, new ArrayList<>(Arrays.asList(s)));
+                } else {
+                    map.get(key).add(s);
+                }
             }
-            String key = s.toString();
-            if (!map.containsKey(s.toString())) map.put(key, new ArrayList<>());
-            List<String> list = map.get(key);
-            list.add(str);
-            map.put(key, list);
-        }
-        for (List<String> value : map.values()) {
-            res.add(value);
+        List<List<String>> res = new ArrayList<>();
+        for (Map.Entry<String, List<String>> entry : map.entrySet()) {
+            res.add(entry.getValue());
         }
         return res;
     }
+
+    private String makeKey(String s) {
+        StringBuilder sb = new StringBuilder();
+        char[] array = s.toCharArray();
+        Arrays.sort(array);
+        for (char c : array) {
+            sb.append(c);
+        }
+        return sb.toString();
+    }
+    
 }
